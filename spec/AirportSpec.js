a7426516df8ad62c1edce3ba4762f27d2a80e175
jose <jennymarin1989@hotmail.com>
2018-02-21 17:49:18 +0000

    Pass stormy conditions test

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -2,11 +2,13 @@ describe("Airport", function() {
 
   var airport;
   var plane;
+  var weather;
 
 
   beforeEach(function() {
-    airport = new Airport();
     plane = new Plane();
+    weather = jasmine.createSpyObj('weather', ['isStormy']);
+    airport = new Airport(weather);
   });
 
   it("should have a default capacity", function() {
@@ -18,34 +20,49 @@ describe("Airport", function() {
     expect(airport.capacity()).toEqual(100);
   });
 
-  it("allows planes to land", function() {
-    airport.land(plane)
-    expect(airport.hangar()).toEqual([plane]);
-  });
+  describe('under normal conditions', function(){
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(false);
+    });
+    it("allows planes to land", function() {
+      airport.land(plane)
+      expect(airport.hangar()).toEqual([plane]);
+    });
 
-  it("allows a plane to take off and confirms take off", function() {
-    airport.land(plane)
-    expect(airport.takeOff(plane)).toEqual("Plane " + plane + " has taken off");
-    expect(airport.hangar()).toEqual([]);
-  });
+    it("allows a plane to take off and confirms take off", function() {
+      airport.land(plane)
+      expect(airport.takeOff(plane)).toEqual("Plane " + plane + " has taken off");
+      expect(airport.hangar()).toEqual([]);
+    });
 
-  it("will not allow a plane to land if capacity has been reached", function(){
-    for (i = 0; i < 50; i++) {
-       airport.land(new Plane);
-    }
-    expect(function() { airport.land(plane) })
-    .toThrow("Airport is at full capacity");
-  });
+    it("will not allow a plane to land if capacity has been reached", function(){
+      for (i = 0; i < 50; i++) {
+         airport.land(new Plane);
+      }
+      expect(function() { airport.land(plane) })
+      .toThrow("Airport is at full capacity");
+    });
 
-  it("will not allow a plane to land if it has already landed", function(){
-    airport.land(plane)
-    expect(function() { airport.land(plane) })
-    .toThrow("That plane has already landed");
-  });
+    it("will not allow a plane to land if it has already landed", function(){
+      airport.land(plane)
+      expect(function() { airport.land(plane) })
+      .toThrow("That plane has already landed");
+    });
 
-  it("will not allow a plane to take off unless it has landed at the airport", function(){
-    expect(function() { airport.takeOff(plane) })
-    .toThrow("That plane is not at this airport");
+    it("will not allow a plane to take off unless it has landed at the airport", function(){
+      expect(function() { airport.takeOff(plane) })
+      .toThrow("That plane is not at this airport");
+    });
   });
-
+  describe('under stormy conditions', function(){
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(true);
+    });
+    it('will not allow a plane to land', function(){
+      expect(function() {airport.land(plane)}).toThrow('it is too stormy to land');
+    });
+    it ('will not allow plane to take off', function(){
+        expect(function() {airport.takeOff(plane)}).toThrow('it is too stormy to take off');
+    });
+  });
 });
--- spec/WeatherSpec.js
+++ spec/WeatherSpec.js
@@ -8,11 +8,11 @@ describe("Weather", function() {
 
   it("sets the weather to sunny", function() {
     spyOn(Math, 'random').and.returnValue(0.1);
-    expect(weather.randomWeather()).toEqual("sunny");
+    expect(weather.isStormy()).toEqual(false);
   });
 
   it("sets the weather to stormy", function() {
     spyOn(Math, 'random').and.returnValue(0.8);
-    expect(weather.randomWeather()).toEqual("stormy");
+    expect(weather.isStormy()).toEqual(true);
   });
 });
